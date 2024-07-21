5b2f8f852ac22b337820c76c8a1851e5c4922280
CodeKiro <kirofiniello@outlook.it>
2024-07-21 16:12:41 +0200

    ho provveduto a prendere i dati dal db e inserirli in pagina, creare delle card abbozzate per visualizzare il contenuto del db

--- laravel-comics/resources/views/Movies/index.blade.php
+++ laravel-comics/resources/views/Movies/index.blade.php
@@ -8,4 +8,26 @@
         Movies List
     </h3>
     {{-- @dump($movies) --}}
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                <div class="cards">
+                    <article class="card">
+                        Titolo : <h1>
+                            {{$movie->title}}
+                        </h1>
+                        Lingua: <h4>
+                        {{ $movie->nationality }}
+                        </h4>
+                        Data: <h4>
+                            {{ $movie->date }}
+                        </h4>
+                        Voto: <h4>
+                            {{ $movie->vote }}
+                        </h4>
+                    </article>
+                </div>
+            </li>
+        @endforeach
+    </ul>
 @endsection
